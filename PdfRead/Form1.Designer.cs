af7674bbb905c1d888b357b7f4bdb2d70fdca971
bhagwan das <bd_chauhan@live.com>
2021-11-26 02:37:23 +0500

    Add files via upload

---
+++ PdfRead/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace PdfRead
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnReadData = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnReadData
+            // 
+            this.btnReadData.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReadData.Location = new System.Drawing.Point(102, 55);
+            this.btnReadData.Name = "btnReadData";
+            this.btnReadData.Size = new System.Drawing.Size(234, 95);
+            this.btnReadData.TabIndex = 0;
+            this.btnReadData.Text = "Read Data";
+            this.btnReadData.UseVisualStyleBackColor = true;
+            this.btnReadData.Click += new System.EventHandler(this.btnReadData_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(720, 575);
+            this.Controls.Add(this.btnReadData);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnReadData;
+    }
+}
+
---
+++ PdfRead/Form1.cs
@@ -0,0 +1,48 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using iText.Kernel.Pdf;
+using iText.Kernel.Pdf.Canvas.Parser;
+using iText.Kernel.Pdf.Canvas.Parser.Listener;
+
+
+
+namespace PdfRead
+{
+    public partial class Form1 : Form
+    {
+
+        string file = @"C:\Users\lal\Documents\Visual Studio 2017\file\Agile Teams.pdf";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnReadData_Click(object sender, EventArgs e)
+        {
+            PdfReader pdfRead = new PdfReader(file);
+            PdfDocument pdfdoc = new PdfDocument(pdfRead);
+
+            for(int page = 1; page <= pdfdoc.GetNumberOfPages(); page++)
+            {
+                ITextExtractionStrategy strategy = new SimpleTextExtractionStrategy();
+
+                string data = PdfTextExtractor.GetTextFromPage(pdfdoc.GetPage(page), strategy);
+                Debug.WriteLine(data);
+                MessageBox.Show("page number : "+page);
+
+            }
+
+        }
+    }
+}
